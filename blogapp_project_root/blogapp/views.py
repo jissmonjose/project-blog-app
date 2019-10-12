d3c2c9c46e4e63a3cc0366a1d31b03ed21514f79
jisshub <jissmon476@gmial.com>
2019-10-12 13:35:56 +0530

    Initial commit added home and about page for my blog app

---
+++ blogapp_project_root/blogapp/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
+
+# Create your views here.
+
+post = [
+    {'author': 'Jacob John',
+     'title': 'Blog Post 1',
+     'content': 'First Post Content',
+     'date': datetime.now(),
+     },
+
+    {'author': 'Remya Shivadas',
+     'title': 'Blog Post 2',
+     'content': 'Second Post Content',
+     'date': datetime.now(),
+     }
+]
+
+
+def home(request, template_name='blogapp/index.html'):
+    return render(request, template_name, {'posts': post, 'title': 'Home'})
+
+
+# next once created the view, then map the urlpattern to this view.
+
+
+def about(request, template_name='blogapp/about.html'):
+    return render(request, template_name, {'title': 'About'})

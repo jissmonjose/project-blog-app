ff1949b052ee0801ec24f1ea137fcf915bab677f
jisshub <jissmon476@gmial.com>
2019-10-16 00:47:31 +0530

    added profile image

--- blogapp_project_root/blogapp_project/urls.py
+++ blogapp_project_root/blogapp_project/urls.py
@@ -17,10 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blogapp.urls')),
     path('user/', include('usersapp.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
-
+# if we are in debug mode add this url to the urlpatterns
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

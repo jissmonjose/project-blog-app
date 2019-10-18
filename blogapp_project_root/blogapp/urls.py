33bbe3d69def2ba6762a1da24d7b5ee06bbb8b7b
jisshub <jissmon476@gmial.com>
2019-10-19 00:36:37 +0530

    added detailview and listview for Post model

--- blogapp_project_root/blogapp/urls.py
+++ blogapp_project_root/blogapp/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blogapp-home'),
+    path('', views.HomeView.as_view(), name='blogapp-home'),
+    path('post/<int:pk>/', views.PostDetails.as_view(), name='blog_post_detail'),
     path('about/', views.about, name='blogapp-about'),
 ]
 
--- blogapp_project_root/blogapp/views.py
+++ blogapp_project_root/blogapp/views.py
@@ -3,13 +3,26 @@ from django.http import HttpResponse
 from .models import Post
 from datetime import datetime
 from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import ListView, DetailView
 
 
 # Create your views here.
-
+# function base views for listing
 def home(request, template_name='blogapp/index.html'):
     return render(request, template_name, {'posts': Post.objects.all(), 'title': 'Home'})
 
+
+# class based view for listing
+class HomeView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    # to order the post from latest to oldest
+    ordering = ['-date']
+
+
+# details view
+class PostDetails(DetailView):
+    model = Post
 
 # next once created the view, then map the urlpattern to this view.
 # here v pass a queryset of POst to post key.

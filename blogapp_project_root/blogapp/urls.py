43814e51d661aab076bf22612b027a9cc4272479
jisshub <jissmon476@gmial.com>
2019-10-21 21:13:35 +0530

    added password_Reset route ans template - reverse march error

--- blogapp_project_root/blogapp/urls.py
+++ blogapp_project_root/blogapp/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='blogapp-home'),
+    path('about/', views.about, name='blogapp-about'),
     path('<str:username>/', views.UserPostView.as_view(), name='users_blog'),
     path('post/<int:pk>/', views.PostDetails.as_view(), name='blog_post_detail'),
-    path('about/', views.about, name='blogapp-about'),
     path('post/create/', views.CreatePost.as_view(), name='blog-post-create'),
     path('post/<int:pk>/update', views.PostUpdate.as_view(), name='blog-post-update'),
     path('post/<int:pk>/delete', views.PostDelete.as_view(), name='blog-post-delete'),
--- blogapp_project_root/blogapp_project/urls.py
+++ blogapp_project_root/blogapp_project/urls.py
@@ -17,11 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blogapp.urls')),
     path('user/', include('usersapp.urls')),
+
 ]
 
 # if we are in debug mode add this url to the urlpatterns
